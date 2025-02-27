61bd5d385cbc8f23b164fea599ea95f791c01f28
Pushti Maheshwari <144027532+pushti07@users.noreply.github.com>
2025-02-27 14:52:49 +0530

    Update Neural_Network.py

--- Neural_Network.py
+++ Neural_Network.py
@@ -370,6 +370,16 @@ print('accuracy : ', accuracy)
 
 
 
+# Need for optimization : 
+import matplotlib.pyplot as plt
+import nnfs
+from nnfs.datasets import vertical_data
+nnfs.init()
+X, y = vertical_data(samples=100, classes=3)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')
+plt.show()
+
+
 
 # Optimization Strategy  1 : Randomly select weights and biases (Doesn't work !)
 X, y = vertical_data(samples=100, classes=3)
